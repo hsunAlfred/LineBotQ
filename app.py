ec006087a4829604d7c7f0751ba836c826d1bb32
hsunAlfred <chenghsungt@gmail.com>
2022-01-12 17:30:42 +0800

    reply message

--- app.py
+++ app.py
@@ -28,10 +28,30 @@ HEADER = {
 }
 
 
+def replyMessage():
+    url = "https://api.line.me/v2/bot/message/reply"
+
+    params = {
+        "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+        "messages": [
+            {
+                "type": "text",
+                "text": "Hello, user"
+            },
+            {
+                "type": "text",
+                "text": "May I help you?"
+            }
+        ]
+    }
+    res = requests.post(url, headers=HEADER, params=params)
+    return res
+
+
 @app.route("/", methods=['POST', 'GET'])
 def index():
     if request.method == 'GET':
-        return 'ok'
+        return replyMessage()
     body = request.json
     events = body["events"]
     print(body)
@@ -53,70 +73,72 @@ def index():
                                            getMRTVideoMessage()]
                 elif text == "扣打":
                     payload["messages"] = [
-                            {
-                                "type": "text",
-                                "text": getTotalSentMessageCount()
-                            }
-                        ]
+                        {
+                            "type": "text",
+                            "text": getTotalSentMessageCount()
+                        }
+                    ]
                 elif text == "今日確診人數":
                     payload["messages"] = [
-                            {
-                                "type": "text",
-                                "text": getTodayCovid19Message()
-                            }
-                        ]
+                        {
+                            "type": "text",
+                            "text": getTodayCovid19Message()
+                        }
+                    ]
                 elif text == "主選單":
                     payload["messages"] = [
-                            {
-                                "type": "template",
-                                "altText": "This is a buttons template",
-                                "template": {
-                                  "type": "buttons",
-                                  "title": "Menu",
-                                  "text": "Please select",
-                                  "actions": [
-                                      {
-                                        "type": "message",
-                                        "label": "我的名字",
-                                        "text": "我的名字"
-                                      },
-                                      {
-                                        "type": "message",
-                                        "label": "今日確診人數",
-                                        "text": "今日確診人數"
-                                      },
-                                      {
-                                        "type": "uri",
-                                        "label": "聯絡我",
-                                        "uri": "tel:0972062449"
-                                      }
-                                  ]
-                              }
+                        {
+                            "type": "template",
+                            "altText": "This is a buttons template",
+                            "template": {
+                                    "type": "buttons",
+                                    "title": "Menu",
+                                    "text": "Please select",
+                                    "actions": [
+                                        {
+                                            "type": "message",
+                                            "label": "我的名字",
+                                            "text": "我的名字"
+                                        },
+                                        {
+                                            "type": "message",
+                                            "label": "今日確診人數",
+                                            "text": "今日確診人數"
+                                        },
+                                        {
+                                            "type": "uri",
+                                            "label": "聯絡我",
+                                            "uri": "tel:0972062449"
+                                        }
+                                    ]
                             }
-                        ]
+                        }
+                    ]
                 else:
                     payload["messages"] = [
-                            {
-                                "type": "text",
-                                "text": text
-                            }
-                        ]
+                        {
+                            "type": "text",
+                            "text": text
+                        }
+                    ]
                 replyMessage(payload)
             elif events[0]["message"]["type"] == "location":
                 title = events[0]["message"]["title"]
                 latitude = events[0]["message"]["latitude"]
                 longitude = events[0]["message"]["longitude"]
-                payload["messages"] = [getLocationConfirmMessage(title, latitude, longitude)]
+                payload["messages"] = [
+                    getLocationConfirmMessage(title, latitude, longitude)]
                 replyMessage(payload)
         elif events[0]["type"] == "postback":
             if "params" in events[0]["postback"]:
-                reservedTime = events[0]["postback"]["params"]["datetime"].replace("T", " ")
+                reservedTime = events[0]["postback"]["params"]["datetime"].replace(
+                    "T", " ")
                 payload["messages"] = [
-                        {
-                            "type": "text",
-                            "text": F"已完成預約於{reservedTime}的叫車服務"
-                        }
-                    ]
+                    {
+                        "type": "text",
+                        "text": F"已完成預約於{reservedTime}的叫車服務"
+                    }
+                ]
                 replyMessage(payload)
             else:
                 data = json.loads(events[0]["postback"]["data"])
@@ -156,7 +178,7 @@ def pretty_echo(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text[::-1])
-        )
+    )
 
 
 @app.route("/sendTextMessageToMe", methods=['POST'])
@@ -191,10 +213,10 @@ def getCarouselMessage(data):
     message["type"] = "template"
     message["altText"] = "this is a image carousel template"
     message["template"] = {
-          "type": "image_carousel",
-          "columns": [
+        "type": "image_carousel",
+        "columns": [
 
-          ]
+        ]
     }
     return message
 
@@ -203,7 +225,8 @@ def getLocationConfirmMessage(title, latitude, longitude):
     message = dict()
     message["type"] = "template"
     message["altText"] = "this is a confirm template"
-    data = {"title": title, "latitude": latitude, "longitude": longitude, "action": "get_near"}
+    data = {"title": title, "latitude": latitude,
+            "longitude": longitude, "action": "get_near"}
     message["template"] = {
 
     }
@@ -216,7 +239,7 @@ def getCallCarMessage(data):
     message["altText"] = "this is a confirm template"
     message["template"] = {
 
-                      }
+    }
     return message
 
 
@@ -281,7 +304,8 @@ def getTotalSentMessageCount():
 
 
 def getTodayCovid19Message():
-    response = requests.get('https://covid-19.nchc.org.tw/api/covid19?CK=covid-19@nchc.org.tw&querydata=4001&limited=TWN')
+    response = requests.get(
+        'https://covid-19.nchc.org.tw/api/covid19?CK=covid-19@nchc.org.tw&querydata=4001&limited=TWN')
     date = response.json()[0]["a04"]
     total_count = response.json()[0]["a05"]
     count = response.json()[0]["a06"]
@@ -309,32 +333,34 @@ def upload_file():
             print(img_path)
             payload["to"] = my_line_id
             payload["messages"] = [getImageMessage(F"{end_point}/{img_path}"),
-                {
-                    "type": "text",
-                    "text": F"年紀：{age}\n性別：{gender}"
-                }
+                                   {
+                "type": "text",
+                "text": F"年紀：{age}\n性別：{gender}"
+            }
             ]
             pushMessage(payload)
     return 'OK'
 
 
-@app.route('/line_login', methods = ['GET'])
+@app.route('/line_login', methods=['GET'])
 def line_login():
     if request.method == 'GET':
         code = request.args.get("code", None)
         state = request.args.get("state", None)
 
         if code and state:
-            #print("code:",code)
-            #print("state:",state)
+            # print("code:",code)
+            # print("state:",state)
             HEADERS = {'Content-Type': 'application/x-www-form-urlencoded'}
             url = "https://api.line.me/oauth2/v2.1/token"
-            FormData = {"grant_type": 'authorization_code', "code": code, "redirect_uri": F"{end_point}/line_login", "client_id": client_id, "client_secret":client_secret}
+            FormData = {"grant_type": 'authorization_code', "code": code,
+                        "redirect_uri": F"{end_point}/line_login", "client_id": client_id, "client_secret": client_secret}
             data = parse.urlencode(FormData)
             content = requests.post(url=url, headers=HEADERS, data=data).text
             content = json.loads(content)
             url = "https://api.line.me/v2/profile"
-            HEADERS = {'Authorization': content["token_type"]+" "+content["access_token"]}
+            HEADERS = {
+                'Authorization': content["token_type"]+" "+content["access_token"]}
             content = requests.get(url=url, headers=HEADERS).text
             content = json.loads(content)
             name = content["displayName"]
@@ -342,9 +368,7 @@ def line_login():
             pictureURL = content["pictureUrl"]
             statusMessage = content["statusMessage"]
             print(content)
-            return render_template('profile.html', name=name, pictureURL=
-                                   pictureURL, userID=userID, statusMessage=
-                                   statusMessage)
+            return render_template('profile.html', name=name, pictureURL=pictureURL, userID=userID, statusMessage=statusMessage)
         else:
             return render_template('login.html', client_id=client_id,
                                    end_point=end_point)
