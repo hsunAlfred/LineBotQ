cc217d14b5175a6bf9d88133afd4e4b9ab80ee57
Hsun <chenghsungt@gmail.com>
2022-01-13 16:29:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -88,9 +88,13 @@ def index():
                                             "text": "扣打"
                                         },
                                         {
-                                            "type": "uri",
-                                            "label": "聯絡我",
-                                            "uri": "tel:0972062449"
+                                           "type":"datetimepicker",
+                                           "label":"Select date",
+                                           "data":"storeId=12345",
+                                           "mode":"datetime",
+                                           "initial":"2017-12-25t00:00",
+                                           "max":"2018-01-24t23:59",
+                                           "min":"2017-12-25t00:00"
                                         }
                                     ]
                             }
